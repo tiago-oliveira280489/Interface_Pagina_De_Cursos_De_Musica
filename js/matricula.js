96998b30aea081e5a0321995ad8418837a1fc2d8
Tiago Antônio Borba Oliveira <tiago.engcomp89@gmail.com>
2021-08-02 14:28:13 -0300

    first commit

---
+++ js/matricula.js
@@ -0,0 +1,31 @@
+var $tdTotalCursos = document.querySelector('.js-total-de-cursos')
+var $tdTotalDeHoras = document.querySelector('.js-total-de-horas')
+var $buttonConfirmar = document.querySelector('.js-botao-matricula')
+var totalHoras = 0
+var totalCursos = 0
+
+function adicionaCursos(checkbox){
+    if (checkbox.checked){
+        totalCursos ++
+        totalHoras += parseInt(checkbox.value)
+    }
+    else{
+        totalCursos --
+        totalHoras -= parseInt(checkbox.value)
+    }
+
+    $tdTotalDeHoras.textContent = totalHoras + ' h'
+    $tdTotalCursos.textContent = totalCursos + ' curso(s)'
+}
+
+$buttonConfirmar.onclick = confirmaMatriculas
+
+function confirmaMatriculas(){
+    if(totalCursos === 0){
+        alert('Nenhum curso selecionado')
+    }
+    else{
+        alert('Matrícula confirmada!')
+        window.location.href = 'index.html'
+    }
+}
